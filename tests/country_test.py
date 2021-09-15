69ed8efb9ab3cfffbb168e0077ad7132edac6b53
judes77 <judyphilip77@btinternet.com>
2021-09-15 18:17:13 +0100

    tests models

--- tests/country_test.py
+++ tests/country_test.py
@@ -4,4 +4,14 @@ from models.country import Country
 class TestCountry(unittest.TestCase):
 
     def setUp(self):
-        self.country = Country("Spain", "Europe", "Spanish")
+        self.country1 = Country("Spain", "Europe", "Spanish")
+        self.country2 = Country("New Zealand", "Oceania", "English")
+
+    def test_country_has_name(self):
+        self.assertEqual("Spain", self.country1.name)
+
+    def test_country_has_language(self):
+        self.assertEqual("Spanish", self.country1.language)
+
+    def test_country_has_continent(self):
+        self.assertEqual("Oceania", self.country2.continent)
