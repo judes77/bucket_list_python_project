6ef418b84ee29e22f9c941c4d4c5bf48273531c8
Judy <judyphilip007@gmail.com>
2021-09-19 12:58:42 +0100

    creates repository functions

--- repositories/city_repository.py
+++ repositories/city_repository.py
@@ -4,6 +4,50 @@ from models.city import City
 from models.country import Country
 import repositories.country_repository as country_repository
 
+
+def save(city):
+    sql = "INSERT INTO cities (name, country_id, visited) VALUES (%s, %s, %s) RETURNING *"
+    values = [city.name, city.country.id, city.visited]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    city.id = id
+    return city
+
+
+def select_all():
+    cities = []
+
+    sql = "SELECT * FROM CITIES"
+    results = run_sql(sql)
+
+    for row in results:
+        country = country_repository.select(row['country_id'])
+        city = City(row['name'], country, row['visited'], row['id'])
+        cities.append(city)
+    return cities
+
+def select(id):
+    city = None
+    sql = "SELECT * FROM tasks WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+    if result is not None:
+        country = country_repository.select(result['country_id'])
+        city = City(result['name'], country, result['visited'], result['id'])
+    return city
+
+def delete(id):
+    sql = "DELETE  FROM cities WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
+
+
 def delete_all():
     sql = "DELETE  FROM cities"
     run_sql(sql)
+
+def update(city):
+    sql = "UPDATE cities SET (name, country_id, completed) = (%s, %s, %s) WHERE id = %s"
+    values = [city.name, city.country.id, city.visited, city.id]
+    run_sql(sql, values)
