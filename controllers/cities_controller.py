005dca278da8ce5aafa36c5b8ce41938b423d4d3
Judy <judyphilip007@gmail.com>
2021-09-20 10:22:21 +0100

    creates edit/update routes

--- controllers/cities_controller.py
+++ controllers/cities_controller.py
@@ -31,6 +31,24 @@ def create_city():
     city_repository.save(city)
     return redirect('/cities')
 
+# EDIT
+@cities_blueprint.route('/cities/<id>/edit')
+def edit_city(id):
+    city = city_repository.select(id)
+    countries = country_repository.select_all()
+    return render_template('cities/edit.html', city = city, all_countries = countries)
+
+# UPDATE
+@cities_blueprint.route('/cities/<id>', methods=['POST'])
+def update_city(id):
+    name = request.form['name']
+    country_id = request.form['country_id']
+    visited = request.form['visited']
+    country = country_repository.select(country_id)
+    city = City(name, country, visited, id)
+    city_repository.update(city)
+    return redirect ('/cities')
+
 # DELETE
 @cities_blueprint.route('/cities/<id>/delete', methods=['POST'])
 def delete_cities(id):
